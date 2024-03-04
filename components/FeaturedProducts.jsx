032be8e504e920f823b04c4a9ec2d2a4f12e66cf
komal paul <149079566+PaulWebdeveloper7@users.noreply.github.com>
2024-03-04 17:01:28 +0530

    slider added

--- components/FeaturedProducts.jsx
+++ components/FeaturedProducts.jsx
@@ -1,24 +1,13 @@
 import React from 'react'
-import Card from   '@components/Card';
-import useFetch from '@src/app/hooks/useFetch.js';
 import '@styles/featuredProducts.scss';
 const featuredProducts = ({type}) => {
-    const {data,loading,error} = useFetch(`Products?populate=*&[filters][type][$eq]=${type}`)
   return (
     <div className='featuredProducts'>
     <div className="top">
        <h1 className='poppins-extralight '>{type} footwear</h1>
        <p className='textanim poppins-extralight '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, accusamus inventore vel voluptate debitis minima deleniti repellat consectetur? </p>
     </div>
-    <div className="bottom">
-    {error
-    ?"Error"
-    :loading
-    ?"loading" 
-    : data?.map(item=>{
-      return  <Card item={item} key={item.id}/>
-      })}
-    </div>
+   
     </div> 
   )
 }
